94496f3f07cd48703d7eb21eac9f0c39b5a257c3
Neve-06 <6255234@rts-365.dk>
2026-03-09 13:45:33 +0100

    added more fetches. For the newsletter and the testimonials. I also have one for assets, but I am currently not using it

--- src/app/lib/dal.js
+++ src/app/lib/dal.js
@@ -18,3 +18,25 @@ export async function getAssets(){
         }
         return await response.json()
 }
+
+export async function newsletter(){
+    const response = await fetch(`http://localhost:4000/api/v1/newsletter/`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+     if (!response.ok) {
+        throw new Error({ message: "something went wrong" })
+    }
+    return await response.json()
+}
+
+export async function getTestimonials(){
+        const response = await fetch(`http://localhost:4000/api/v1/testimonials/`)
+    
+    if (!response.ok) {
+        throw new Error({ message: "something went wrong" })
+    }
+    return await response.json()
+}
