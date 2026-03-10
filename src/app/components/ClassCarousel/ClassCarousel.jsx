983bb99a6cb8725defcdcebb7dfc2d97441e62fa
Neve-06 <6255234@rts-365.dk>
2026-03-10 14:13:12 +0100

    carousel component

---
+++ src/app/components/ClassCarousel/ClassCarousel.jsx
@@ -0,0 +1,47 @@
+"use client"
+import React from "react"
+import Link from "next/link"
+import useEmblaCarousel from "embla-carousel-react"
+import Image from "next/image"
+import Autoplay from "embla-carousel-autoplay"
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+
+
+export default function ClassCarousel({ classes = [] }) {
+    const [emblaRef] = useEmblaCarousel()
+
+
+    // const scrollPrev = () => emblaApi?.scrollPrev()
+    // const scrollNext = () => emblaApi?.scrollNext()
+
+    return (
+        <>
+            <div className="embla">
+                <h2 className="embla__container-title">Classes for you</h2>
+                <div className="embla__viewport" ref={emblaRef}>
+                    <div className="embla__container">
+
+                        {classes.map((classData) => (
+                            <Link className="embla__slide" href={`/classes/${classData.id}`}>
+                            <article >
+                                <p>{classData.className}</p>
+                                <figure>
+                                    <img  key={classData.id} loading="lazy" src={classData.asset.url} alt={classData.className}></img>
+                                </figure>
+                            </article>
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+                {/* <div className="embla__button-container">
+                    <button className="embla__button">
+                        <IoIosArrowBack style={{ color: "#ffffff" }} onClick={scrollPrev} />
+                    </button>
+                    <button className="embla__button">
+                        <IoIosArrowForward style={{ color: "#ffffff" }} onClick={scrollNext} />
+                    </button>
+                </div> */}
+            </div>
+        </>
+    )
+}
