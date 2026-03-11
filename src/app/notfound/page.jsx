9ef1758868808ec678ec5b3f6ef879921d931755
Neve-06 <6255234@rts-365.dk>
2026-03-11 15:35:10 +0100

    created proxy, notfound and no-acess pages

---
+++ src/app/notfound/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default function page() {
+
+    return (
+        <>
+        <main>
+            <h1>Oops!</h1>
+            <p>It seems like the page you are looking for does not exist.</p>
+        </main>
+        </>
+    )
+}
